2c09a2636eb208a2811bc703cc9f96338b03ce22
Aus Dvl <admin@blockid.au>
2026-05-21 16:39:31 +0000

    fix: email sender BlockID.au <info@blockid.au>

--- web/src/lib/email.ts
+++ web/src/lib/email.ts
@@ -17,7 +17,7 @@ import {
   getPreferencesUrl,
 } from "./email-preferences";
 
-const FROM_DEFAULT = "BlockID <info@blockid.au>";
+const FROM_DEFAULT = "BlockID.au <info@blockid.au>";
 
 function fromAddress(): string {
   return process.env.SMTP_FROM_EMAIL || FROM_DEFAULT;
